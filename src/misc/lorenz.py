73a42649925756f28053605ba210b50e912f123c
neozhaoliang <mathzhaoliang@gmail.com>
2017-08-31 09:57:06 +0800

    change default lorenz video size to 1200x960

--- src/misc/lorenz.py
+++ src/misc/lorenz.py
@@ -86,5 +86,8 @@ def animate(i):
 anim = FuncAnimation(fig, animate, init_func=init, interval=5, 
                      frames=500, blit=True)
 #fig.show()
-anim.save('lorenz.webm', writer='ffmpeg', fps=30, dpi=100,
+
+# Note: this `dpi` has nothing to do with the dpi settings in `plt.figure()`,
+# it determines the size of the video together with `figsize`.
+anim.save('lorenz.webm', writer='ffmpeg', fps=30, dpi=200,
           bitrate=1000, codec='libvpx', extra_args=['-crf', '10'])
